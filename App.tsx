c4f0f62dc6120a1c11a8a066100a8a1727f1bbbb
Tomas Felipe <aplications.ec@gmail.com>
2025-05-03 18:39:00 -0500

    test

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import AppProviders from "./src/providers/AppProviders";
 import RootNavigator from "./src/navigation/RootNavigator";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
   return (
     <AppProviders>
       <RootNavigator />
+      <StatusBar backgroundColor="black" />
     </AppProviders>
   );
 }
--- src/navigation/DrawerNavigator.tsx
+++ src/navigation/DrawerNavigator.tsx
@@ -12,16 +12,41 @@ const Drawer = createDrawerNavigator<DrawerParamList>();
 
 export default function DrawerNavigator() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: "black", // 🎨 Fondo del drawer
+        },
+        drawerActiveTintColor: "#fff", // ✅ Color del texto activo
+        drawerInactiveTintColor: "#ccc", // ✅ Color del texto inactivo
+        drawerActiveBackgroundColor: "#1f1f1f", // ✅ Fondo del ítem activo
+      }}
+    >
       <Drawer.Screen
         options={{
           title: "Inicio",
           headerShown: true,
+          headerStyle: {
+            backgroundColor: "black",
+          },
+          headerTintColor: "white",
         }}
         name="Home"
         component={HomeScreen}
       />
-      <Drawer.Screen name="Profile" component={ProfileScreen} />
+      <Drawer.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          title: "Inicio",
+          headerShown: true,
+          headerStyle: {
+            backgroundColor: "black",
+          },
+          headerTintColor: "white",
+        }}
+      />
     </Drawer.Navigator>
   );
 }
